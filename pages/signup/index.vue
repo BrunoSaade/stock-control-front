1f54814c1c5f1746d070be0f0ddf1636b2b45b4c
BrunoSaade <brsaade@gmail.com>
2024-05-10 12:08:16 -0300

    Feat: Signup page created

---
+++ pages/signup/index.vue
@@ -0,0 +1,74 @@
+<template lang="pug">
+.l-stack-center-center.w-full
+  v-card(class="w-[540px]")
+    v-card-title Sign up
+    v-card-text
+      v-form(@submit.prevent='submitForm')
+        v-text-field(v-model='email' label='Email' type='email')
+        v-text-field(v-model='password' label='Password' type='password')
+        v-text-field(v-model='confirmPassword' label='Confirm Password' type='password')
+        v-btn(
+          type='submit' 
+          color="primary"
+          class="w-[240px] !bg-secondary-200"
+        ) Sign up
+  p.mt-4.text-s3 Already signed up? 
+    NuxtLink.text-tertiary-200.underline(to="/") Login!
+</template>
+
+<script>
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex"
+import * as getter_types from "@/store/types/getter-types"
+import * as mutation_types from "@/store/types/mutation-types"
+import * as action_types from "@/store/types/action-types"
+export default {
+  name: "Index",
+  layout: "Default",
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState({
+      authSignup: (state) => state.auth?.signup,
+    }),
+    email: {
+      get() {
+        return this.authSignup?.email
+      },
+      set(value) {
+        this.setSignupEmail(value)
+      },
+    },
+    password: {
+      get() {
+        return this.authSignup?.password
+      },
+      set(value) {
+        this.setSignupPassword(value)
+      },
+    },
+    confirmPassword: {
+      get() {
+        return this.authSignup?.confirmPassword
+      },
+      set(value) {
+        this.setSignupConfirmPassword(value)
+      },
+    }
+  },
+  watch: {},
+  mounted() {},
+  created() {},
+  methods: {
+    ...mapActions({}),
+    ...mapMutations({
+      setSignupEmail: mutation_types.SET_EMAIL_SIGNUP,
+      setSignupPassword: mutation_types.SET_PASSWORD_SIGNUP,
+      setSignupConfirmPassword: mutation_types.SET_CONFIRM_PASSWORD_SIGNUP
+    }),
+    submitForm() {
+    },
+  }
+}
+</script>
