f3ce62c7178335bb4baf6645d775ec20ec7e4ff0
BrunoSaade <brsaade@gmail.com>
2024-05-11 21:07:16 -0300

    Feat: Logou button added

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,11 +1,35 @@
 <template lang="pug">
-v-container.l-inline-center-center.p-5(class="w-[100vh] h-[100vh]")
+v-container.l-inline-center-start.p-5(class="w-[100vh] h-[100vh]")
   nuxt
+  v-btn.bottom-0.right-0.m-5(
+    v-if="isLogged"
+    class="logout-button" 
+    fab dark 
+    color="primary" 
+    @click="logout"
+  )
+    v-icon dark mdi-logout
 </template>
 
 <script>
 export default {
   name: "Default",
+  computed: {
+    isLogged() {
+      return this.$route.path.includes("home")
+    }
+  },
+  methods: {
+    logout() {
+      this.$router.push('/')
+    }
+  }
 }
 </script>
+
+<style scoped>
+.logout-button {
+  position: fixed;
+}
+</style>
   
